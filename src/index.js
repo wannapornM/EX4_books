3b6a51159a48974d1135e2c40f175762eb5eab95
wannapornM <wannaporndangkum@gmail.com>
2021-07-05 16:18:51 +0700

    Modify javascript

--- src/index.js
+++ src/index.js
@@ -2,5 +2,24 @@ import './main.scss'
 import booksCollection from './books.json'
 
 document.addEventListener('DOMContentLoaded', function(){
-    console.log(booksCollection)
+    const booksCate = document.getElementById('books-category') 
+    const books = booksCollection.data
+
+    books.forEach(book => {
+        let bookDOM = buildBookDOM(book)
+        booksCate.insertAdjacentHTML('beforeend', bookDOM)
+    });
 })
+
+function buildBookDOM(book) {
+    let imgbook = booksCollection.image_base_url
+
+    return `<div class="book">
+                <img src="${imgbook.concat(`${book.ImageURL}`)}">
+                <div class="details">
+                    <h4>${book.Title}</h4>
+                    <p>${book.Authors}</p>
+                    <p>${book.Format}</p>
+                </div>
+            </div>`
+}
